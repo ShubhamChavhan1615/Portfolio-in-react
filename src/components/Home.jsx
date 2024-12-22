6ac593c344b83e6cf2a11769d81a3eba13988920
Shubham Chavhan <shubhamchavhan908@gmail.com>
2024-12-22 15:33:59 +0530

    update resume file path to include full name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { SiMongodb, SiExpress } from "react-icons/si";
 import { FaReact, FaNodeJs } from "react-icons/fa6";
 import { ReactTyped } from "react-typed";
 
-import Resume from "../../public/shubhamResume.pdf";
+import Resume from "../../public/Shubham Chavhan full-stack Resume.pdf";
 
 function Home() {
   return (
